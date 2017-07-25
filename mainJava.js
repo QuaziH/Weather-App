857d8459ec225b1fac6d3f450d161ee1b15d228a
Quazi <q.hossain@ymail.com>
2017-07-25 11:49:55 -0400

    working api

--- mainJava.js
+++ mainJava.js
@@ -1,11 +1,20 @@
-/**
- * Created by syedjavaid on 7/25/17.
- */
-
-
-// Only change code below this line.
 if (navigator.geolocation) {
+    var api_url;
     navigator.geolocation.getCurrentPosition(function(position) {
         $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+        api_url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=652d6f41ad0cb28b749ec584af19bddd';
+        $.ajax({
+            url : api_url,
+            method : 'GET',
+            success : function (data) {
+                var tempr = data.main.temp;
+                var location = data.name;
+                var desc = data.weather.description;
+
+                $('#result').text(tempr + '° ' + location);
+
+            }
+        });
     });
 }
+
