72d0218b298d43b95ee40c00b33ad7d487fbb6aa
jackreaper121 <s.java458@gmail.com>
2017-08-02 14:33:34 -0400

    My location button and javascript for search fixed

--- mainJava.js
+++ mainJava.js
@@ -1,48 +1,107 @@
+var myTempr;
+var myArea;
+var myDesc;
+
 if (navigator.geolocation) {
     var api_url;
     navigator.geolocation.getCurrentPosition(function(position) {
-        $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
         api_url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
         $.ajax({
             url : api_url,
             method : 'GET',
             success : function (data) {
-                var tempr = data.main.temp;
-                var location = data.name;
-                var desc = data.weather.description;
+                 myTempr = data.main.temp;
+                 myArea = data.name;
+                 myDesc = data.weather.description;
 
-                // $('#result').text(tempr + '° '  + location);
+                 // $('#result').text(tempr + '° '  + location);
 
             }
         });
     });
 }
+function myLocation(){
 
+    $('#result').text(myTempr + '° '  + myArea);
+
+
+}
+
+$("#place").on("keydown",function search(e) {
+    if(e.keyCode == 13) {
+        myLocationInput(document.getElementById('place').value);
+    }
+});
+function search(ele) {
+    if(event.keyCode == 13) {
+        alert(ele.value);
+    }
+
+}
 function myLocationInput(location) {
 
-    var locationToRun = location;
-    $('#test').text(locationToRun);
-    console.log(locationToRun);
-    // var city_name =  locationToRun.substr(0,locationToRun.indexOf(' '));
-    // var country_code = locationToRun.substr(locationToRun.indexOf(' ' + 1));
-    // console.log(locationToRun);
-    // if(city_name == true && country_code == true){
-    //     var api_url = 'http://api.openweathermap.org/data/2.5/weather?q={'+ city_name +'},{'+ country_code +'}&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
-    //
-    // $.ajax({
-    //     url: api_url,
-    //     method: 'GET',
-    //
-    //     success: function () {
-    //         var tempr = locationToRun.main.temp;
-    //         var location = locationToRun.name;
-    //         var desc = locationToRun.weather.description;
-    //
-    //         $('#result').text(tempr + '° ' + location);
-    //
-    //     }
-    // });
-    // }
+    if (isNaN(location) == false) {
+        var api_url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + location + '&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
+        $.ajax({
+            url: api_url,
+            method: 'GET',
+
+            success: function (data) {
+                var tempr = data.main.temp;
+                var location = data.name;
+                var desc = data.weather.description;
+
+                $('#result').text(tempr + '° ' + location);
+
+            }
+        });
+    }
+    if (location.indexOf(',') > -1) {
+        var city_name = location.substr(0, location.indexOf(' '));
+        var country_code = location.substr(location.indexOf(' ' + 1));
+
+
+        var api_url = 'http://api.openweathermap.org/data/2.5/weather?q={' + city_name + '},{' + country_code + '}&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
+
+        $.ajax({
+            url: api_url,
+            method: 'GET',
+
+            success: function (data) {
+                var tempr = data.main.temp;
+                var location = data.name;
+                var desc = data.weather.description;
+
+                $('#result').text(tempr + '° ' + location);
+
+            }
+        });
+    }
+    else {
+
+        var api_url = 'http://api.openweathermap.org/data/2.5/weather?q={' + location + '}&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
+
+        $.ajax({
+            url: api_url,
+            method: 'GET',
+
+            success: function (data) {
+                var tempr = data.main.temp;
+                var location = data.name;
+                var desc = data.weather.description;
+
+                $('#result').text(tempr + '° ' + location);
+
+            }
+        });
+
+
+    }
+}
+
+function ClearFields() {
+
+    document.getElementById("place").value = "";
 
 }
 
