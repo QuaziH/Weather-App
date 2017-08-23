2e59088f84c97c6e1222636d4bb599ab5500a8cc
Quazi <q.hossain@ymail.com>
2017-08-23 00:09:31 -0400

    cleaned up the style and used bootstrap for the form

--- js/script.js
+++ js/script.js
@@ -3,31 +3,27 @@ var currentArea;
 var currentMain;
 var currentIcon;
 
-
-  
 function myLocation(){
+    var api_url;
+    navigator.geolocation.getCurrentPosition(function(position) {
+        api_url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
 
-        var api_url;
-        navigator.geolocation.getCurrentPosition(function(position) {
-            api_url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=imperial&appid=652d6f41ad0cb28b749ec584af19bddd';
-            $.ajax({
-                url : api_url,
-                method : 'GET',
-                success : function (data) {
-                    currentTemp = data.main.temp;
-                    currentArea = data.name;
-                    currentMain = data.weather[0].main;
-                    currentIcon = data.weather[0].icon;
+        $.ajax({
+            url : api_url,
+            method : 'GET',
 
-                    $('#weather').text(currentTemp + '° ');
-                    $('#icon').attr("src",'http://openweathermap.org/img/w/' + currentIcon + '.png');
-                    $('#location').text(currentArea);
-                }
-            });
+            success : function (data) {
+                currentTemp = data.main.temp;
+                currentArea = data.name;
+                currentMain = data.weather[0].main;
+                currentIcon = data.weather[0].icon;
+
+                $('#weather').text(currentTemp + '° ');
+                $('#icon').attr("src",'http://openweathermap.org/img/w/' + currentIcon + '.png');
+                $('#location').text(currentArea);
+            }
         });
-
-
-
+    });
 }
 
 function myLocationInput(location) {
